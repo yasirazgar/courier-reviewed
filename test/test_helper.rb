29ea45e51bce8b6243bc04f2deb208bc6f61ddcd
yasirazgar <yasirazgar@gmail.com>
2019-09-15 14:34:13 +0530

    add mocha for mocking/stubbing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'mocha/minitest'
 require_relative 'helpers/requests_helper'
 
 class ActiveSupport::TestCase
