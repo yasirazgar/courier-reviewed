c9c075d7a2e8ddcce3ebeaf8acdaa7bf09fa8c60
yasirazgar <yasirazgar@gmail.com>
2019-09-15 14:14:39 +0530

    more refactor to url test_helper keeping it simple now

--- test/helpers/requests_helper.rb
+++ test/helpers/requests_helper.rb
@@ -2,21 +2,21 @@ require 'json_web_token'
 module RequestsHelper
   def json_post(url, user, params={})
     post(
-      send(url, format: :json),
+      url,
       headers: { "Authorization" => token_for_user(user) },
       params: params)
   end
 
   def json_patch(url, user, params={})
     patch(
-      send(url, format: :json),
+      url,
       headers: { "Authorization" => token_for_user(user) },
       params: params)
   end
 
   def json_get(url, user, params={})
     get(
-      send(url, format: :json),
+      url,
       headers: { "Authorization" => token_for_user(user) },
       params: params)
   end
