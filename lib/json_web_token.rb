6d720a15d09a7abe38706e83ea958b6ac742ad60
yasirazgar <yasirazgar@gmail.com>
2019-09-16 23:48:58 +0530

    use secret from env variable

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -5,11 +5,11 @@ class JsonWebToken
 
   def self.encode(payload, exp = DEFAULT_AUTH_TOKEN_EXP)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
-    body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+    body = JWT.decode(token, ENV['SECRET_KEY_BASE'] || Rails.application.secrets.secret_key_base)[0]
     HashWithIndifferentAccess.new body
   rescue
     {}
